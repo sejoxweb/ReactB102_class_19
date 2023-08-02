33d36eb0159a7d9bf1aa44b7bc41d6f3562b8f3d
jafar1405 <jafar1405@gmail.com>
2023-08-02 07:28:09 +0530

    usestate, props, component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import Output from "./components/Output";
+
 function App() {
-  return <div>hello world, here is iam</div>;
+  const [inputValue, setInputValue] = useState(""); //"" [state, setState]
+  const [showValue, setShowValue] = useState("");
+
+  const handleChange = (e) => {
+    //console.log("value>>>", e.target.value);
+    //inputValue = e.target.value;
+    setInputValue(e.target.value);
+  };
+
+  // console.log("inputValue>>>", inputValue);
+
+  const handleClick = () => {
+    setShowValue(inputValue);
+  };
+
+  return (
+    <div>
+      <input onChange={handleChange} />
+      <button onClick={handleClick}>Click me</button>
+      {/* <div>here: {showValue}</div> */}
+      {/* properties to be called as "props" */}
+      <Output value={showValue} here="here:" />
+    </div>
+  );
 }
 
 export default App;
