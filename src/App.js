f843e3bcb459eb7059f1e8d92a93359e6b1b650f
jafar1405 <jafar1405@gmail.com>
2023-08-04 07:05:12 +0530

    product list

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Output from "./components/Output";
 import Input from "./components/Input";
 import Button from "./components/Button";
 import InputTwo from "./components/InputTwo";
+import ProductsList from "./components/ProductsList";
 
 function App() {
   const [inputValue, setInputValue] = useState(""); //"" [state, setState]
-  const [showValue, setShowValue] = useState("");
-
+  //const [showValue, setShowValue] = useState("");
+  const [products, setProducts] = useState(["iphone", "samsung"]);
+  console.log("products>>>", products);
   const handleChange = (e) => {
     //console.log("value>>>", e.target.value);
     //inputValue = e.target.value;
@@ -17,8 +19,18 @@ function App() {
   // console.log("inputValue>>>", inputValue);
 
   const handleClick = () => {
-    setShowValue(inputValue);
+    // setShowValue(inputValue);
+    //1st aproach
+    // const tempProducts = [...products];
+    // tempProducts.push(inputValue);
+    // console.log("sdfsfd", tempProducts);
+    // setProducts(tempProducts);
+    //2nd aproach
+    setProducts([...products, inputValue]);
   };
+
+  // const product1 = "iphone";
+  // const product2 = "samsung";
 
   return (
     <div>
@@ -28,8 +40,10 @@ function App() {
       {/* <button onClick={handleClick}>Click me</button> */}
       {/* <div>here: {showValue}</div> */}
       {/* properties to be called as "props" */}
-      <Output value={showValue} here="here:" />
-      <InputTwo />
+      {/* <Output value={showValue} here="here:" /> */}
+      {/* <InputTwo /> */}
+      {/* products here */}
+      <ProductsList products={products} />
     </div>
   );
 }
