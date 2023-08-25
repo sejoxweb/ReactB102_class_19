45a2c152c445c496e95b330417e0f3a7eb6d7ec5
jafar1405 <jafar1405@gmail.com>
2023-08-25 07:03:39 +0530

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -12,11 +12,29 @@ import CustomHookExampleTwo from "./components/CustomHookExampleTwo";
 import UseRefVsUseMemo from "./components/UseRefVsUseMemo";
 
 import css from "./test.module.css";
+import Layout from "./pages/Layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NoPage from "./pages/NoPage";
 
 function App() {
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const apiKeyProd = process.env.REACT_APP_API_KEY_PROD;
   return (
     <div>
-      <UseRefVsUseMemo />
+      {process.env.DEV ? apiKey : apiKeyProd}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />}></Route>
+            <Route path="about/test" element={<div>test</div>} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <UseRefVsUseMemo /> */}
       {/* <CustomHookExampleTwo /> */}
       {/* <CustomHookExample /> */}
       {/* <UseRefExampleThree /> */}
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
